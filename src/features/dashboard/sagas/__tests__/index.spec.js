d4e91965dbd84bae9860aeb7c9d0d48309f8b19e
Ionut Cirja <ionut.cirja@gmail.com>
2019-10-24 11:57:26 +0100

    create dashboard sagas / add fetchCarbonIntensityRequest listener

---
+++ src/features/dashboard/sagas/__tests__/index.spec.js
@@ -0,0 +1,55 @@
+import { call, takeLatest, put } from 'redux-saga/effects';
+import {
+  fetchCarbonIntensity,
+} from '../../services';
+import {
+  watchDashboardRequests,
+  fetchCarbonIntensityRequest,
+} from '..';
+
+describe('Dashboard sagas', () => {
+  describe('watchDashboardRequests', () => {
+    it('should wait for a FETCH_CARBON_INTENSITY_REQUEST action', () => {
+      const generator = watchDashboardRequests();
+      expect(generator.next().value).toEqual(
+        takeLatest('FETCH_CARBON_INTENSITY_REQUEST', fetchCarbonIntensityRequest),
+      );
+      expect(generator.next().done).toEqual(true);
+    });
+  });
+
+  describe('fetchCarbonIntensityRequest', () => {
+    it('should call fetchCarbonIntensity services method and'
+      + ' dispatch a FETCH_CARBON_INTENSITY_SUCCESS action in case of success', () => {
+      const data = {
+        from: '24/10/2019',
+        to: '25/10/2019',
+      };
+      const generator = fetchCarbonIntensityRequest();
+      expect(generator.next().value).toEqual(call(fetchCarbonIntensity));
+      expect(generator.next(data).value).toEqual(
+        put({
+          type: 'FETCH_CARBON_INTENSITY_SUCCESS',
+          payload: { data },
+        }),
+      );
+      expect(generator.next().done).toEqual(true);
+    });
+
+    it('should call fetchCarbonIntensity services method and'
+      + ' dispatch a FETCH_CARBON_INTENSITY_ERROR action in case of error', () => {
+      const error = { message: 'error' };
+      const generator = fetchCarbonIntensityRequest();
+      expect(generator.next().value).toEqual(call(fetchCarbonIntensity));
+      expect(generator.throw(error).value).toEqual(
+        put({
+          type: 'FETCH_CARBON_INTENSITY_ERROR',
+          payload: {
+            error: true,
+          },
+        }),
+      );
+      expect(generator.next().done).toEqual(true);
+    });
+  });
+});
---
+++ src/features/dashboard/sagas/index.ts
@@ -0,0 +1,21 @@
+import { SagaIterator } from 'redux-saga';
+import { takeLatest, call, put } from 'redux-saga/effects';
+import {
+  FETCH_CARBON_INTENSITY_REQUEST,
+  fetchCarbonIntensitySuccess,
+  fetchCarbonIntensityError,
+} from '../actions';
+import { fetchCarbonIntensity } from '../services';
+
+export function* fetchCarbonIntensityRequest(): SagaIterator {
+  try {
+    const data = yield call(fetchCarbonIntensity);
+    yield put(fetchCarbonIntensitySuccess({ data }));
+  } catch (error) {
+    yield put(fetchCarbonIntensityError({ error: true }));
+  }
+}
+
+export function* watchDashboardRequests(): SagaIterator {
+  yield takeLatest(FETCH_CARBON_INTENSITY_REQUEST, fetchCarbonIntensityRequest);
+}
