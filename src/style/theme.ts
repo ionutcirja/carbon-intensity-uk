6c9ceea89df3b9c0849edf27258a5f4e8afd148c
Ionut Cirja <ionut.cirja@gmail.com>
2019-10-26 10:23:06 +0100

    create style theme

---
+++ src/style/theme.ts
@@ -0,0 +1,13 @@
+import { Theme } from 'styled-components';
+
+const theme: Theme = {
+  colors: {
+    darkGrey: '#111111',
+    grey: '#9a9a9a',
+    blue: '#7eb0d5',
+    darkBlue: '#3f5a77',
+    red: '#ef705f',
+  },
+};
+
+export default theme;
