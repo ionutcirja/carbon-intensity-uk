5ea8b5cd4bd6eb5ca3473ef2dd1198529b1cbbf3
Ionut Cirja <ionut.cirja@gmail.com>
2019-10-26 09:58:48 +0100

    add style linting

--- src/style/index.ts
+++ src/style/index.ts
@@ -164,7 +164,6 @@ const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased;
     font-family: 'Helvetica', sans-serif;
     font-size: 10px;
-    line-height: 16px;
   }
   
   *:focus {
